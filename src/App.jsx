664df0aa23ca7c3116c24d81478c4fdf71c86c1d
nelson1234 <okehnelsonchidera@gmail.com>
2026-08-07 15:13:34 +0100

    Added Hero + Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css';
 import Stats from './Projects-page/Stats/Stats';
 import Testimonials from './Projects-page/Testimonials/Testimonials';
-
+import Hero from './Projects-page/hero/hero';
+import Footer from './Projects-page/footer/footer';
 function App() 
 {
 	return (
 		<>
 			<Stats />
 			<Testimonials />
+			<Hero/>
+			<Footer/>
 		</>
 	);
 }
-
 export default App
