b8a81aabbca56284cdd67b939aa77bc6911d6ced
haleemahsadiq6-art <haleemahsadiq6@gmail.com>
2026-08-12 11:02:10 +0100

    Fix service page

--- src/Services-page/components/Services-page.jsx
+++ src/Services-page/components/Services-page.jsx
@@ -3,6 +3,11 @@
 import React from 'react';
 import Hero from './components/hero';
 import Core from './core';
+import ConsultingCard from './ConsultingCard';
+import SustainabilityCard from './SustainabilityCard';
+import ProcessSection from './ProcessSection';
+import CTASection from './CTASection';
+import Footer from './Footer';
 function App() {
   return (
     <main className="w-full min-h-screen bg-[#0A0D14]">
@@ -10,6 +15,15 @@ function App() {
       <Hero /> 
     
     <core/>
+    <ConsultingCard/>
+    <SustainabilityCard/>
+    <ProcessSection/>
+    <CTASection/>
+    <Footer/>
+    
+
+
+
     </main>
 
   
